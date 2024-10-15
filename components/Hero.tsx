e297cddc81aab3c5d8cf86b8c88d543c2cfe1246
MUHAMMAD OSAMA <developerscripters@gmail.com>
2024-10-14 22:24:18 -0700

    first

---
+++ components/Hero.tsx
@@ -0,0 +1,58 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link';
+import React from 'react'
+
+
+import { Typewriter,  } from 'react-simple-typewriter';
+const Hero = () => {
+  return (
+    <div id='home' className='px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32'>
+      <div className='flex flex-col items-center justify-center gap-10 text-white'>
+        <div>
+          <Image
+          src="/myphoto.jpg"
+          alt='Osama'
+          height={500}
+          width={500} 
+          className='w-[300px] cursor-pointer rounded-full shadow-xl shadow-red-600 transition-all duration-300
+          hover:shadow-indigo-600 md:w[350px]'
+          />
+        </div>
+
+        <div className='flex max-w-[600px] flex-col items-center justify-center gap-3 text-center relative' >
+          <h3 className='bg-gradient-to-r from-indigo-600 to-red-600 bg-clip-text text-transparent text-2xl md:text-3xl'>السلام علیکم</h3>
+          <p className='absolute bottom-[90%] right-[76%] text-[22px]'>My Self,</p>
+          <h1 className='bg-gradient-to-r from-red-600 to-indigo-600 bg-clip-text text-transparent
+            text-5xl font-bold md:text-7xl transition-all duration-600'>MUHAMMAD OSAMA</h1>
+
+            <h3 className="text-white text-2xl font-bold">
+                 {' '}
+                <span className="bg-gradient-to-r from-indigo-600 to-red-600 p-2 rounded-md">
+                    <Typewriter
+                        words={['Software Engineer','Web Developer','Data Analyst', 'Machine learning Expert']}
+                        loop={Infinity}
+                        cursor
+                        cursorStyle="|" 
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                    />
+                </span>
+            </h3>
+
+
+
+          <div className='md:text-base text-pretty text-sm text-gray-400'>
+          <p >As a Software Engineer, I specialize in web development and data analysis, leveraging a robust skill set in modern technologies to deliver innovative solutions. My  expertise lies in crafting dynamic and responsive user interfaces using <span className='text-pink-600'>React.js</span> and <span className='text-pink-600'>Next.js</span>, complemented by <span className='text-pink-600'><Link href='https://tailwindcss.com/' target='_blank'>Tailwind Css</Link></span> for streamlined styling. I excel in creating  engaging animations and transitions using <span className='text-pink-600'>Framer Motion</span> and <span className='text-pink-600'>GSAP</span>, enhancing user experience through interactive design.
+          </p>
+
+          <p className='pt-2'>In addition to my front-end development skills, I possess a solid foundation in <span className='text-pink-600'>data analysis</span> and <span className='text-pink-600'>machine learning</span>. I utilize analytical techniques and machine learning   algorithms to extract insights from complex datasets, enabling data-driven decision-making. My ability to bridge the gap between software engineering and data science  allows me to contribute effectively to cross-functional teams, driving project success and fostering continuous improvement.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/Navbar.tsx
@@ -0,0 +1,90 @@
+'use client';
+import React, { useState } from 'react';
+import { BiMenu, BiX } from 'react-icons/bi';
+import { BsGithub, BsLinkedin, BsWhatsapp } from 'react-icons/bs';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const menuOpen = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className='fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly'>
+            <a href='#home' className='bg-gradient-to-r from-red-600 to-indigo-600 bg-clip-text text-transparent
+             opacity-80 text-3xl font-semibold transition-all duration-300 hover:opacity-100'>
+                Osama
+            </a>
+
+            <ul className='hidden md:flex gap-10'>
+                <a href="#home" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                    <li>Home</li>
+                </a>
+                <a href="#tech" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                    <li>Tech</li>
+                </a>
+                <a href="#projects" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                    <li>Projects</li>
+                </a>
+                <a href="#contact" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                    <li>Contact</li>
+                </a>
+            </ul>
+
+            <ul className='hidden md:flex gap-5'>
+                <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-green-500 hover:bg-opacity-100'>
+                    <BsWhatsapp />
+                </li>
+                <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-gray-400 hover:bg-opacity-100'>
+                    <BsGithub />
+                </li>
+                <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:bg-opacity-100'>
+                    <BsLinkedin />
+                </li>
+            </ul>
+
+            {/* Hide the menu icons on md and larger screens */}
+            <div className='md:hidden'>
+                {isOpen ? (
+                    <BiX onClick={menuOpen} />
+                ) : (
+                    <BiMenu onClick={menuOpen} />
+                )}
+            </div>
+
+            {isOpen && (
+                <div className={`fixed right-0 top-[100px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-1 border-gray-800 bg-black/90 p-12 ${isOpen ? 'block' : 'hidden'}`}>
+                    <ul className='flex flex-col gap-8'>
+                        <a href="#home" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                            <li>Home</li>
+                        </a>
+                        <a href="#tech" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                            <li>Tech</li>
+                        </a>
+                        <a href="#projects" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                            <li>Projects</li>
+                        </a>
+                        <a href="#contact" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>
+                            <li>Contact</li>
+                        </a>
+                    </ul>
+
+                    <ul className='flex flex-wrap gap-5'>
+                        <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-green-500 hover:bg-opacity-100'>
+                            <BsWhatsapp />
+                        </li>
+                        <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-purple-500 hover:bg-opacity-100'>
+                            <BsGithub />
+                        </li>
+                        <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:bg-opacity-100'>
+                            <BsLinkedin />
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Navbar;
